83fca9c2d1e9cbed227e69aecb1b868e9acb5c1d
pfcui <cuipengfei2008@gmail.com>
2016-10-18 15:34:37 +0800

    java doc

--- openlmis-mobile-core/src/main/java/org/openlmis/mobile/Sanity.java
+++ openlmis-mobile-core/src/main/java/org/openlmis/mobile/Sanity.java
@@ -1,8 +1,15 @@
 package org.openlmis.mobile;
 
+/**
+ * This is just a sanity check class to make sure the project set up is ok.
+ */
 public class Sanity {
     //this class should have a class file in the aar file generated
 
+    /**
+     * Constructor for project set up.
+     * @param a this is just for test coverage purposes.
+     */
     public Sanity(int a) {
         int b = 2;
         if (a == b) {
